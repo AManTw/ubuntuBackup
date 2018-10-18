d3ea6c8bf11f65cb393b8960662891e64ee43e3f
AManTw <jerr83553@gmail.com>
2018-10-18 11:13:11 +0800

    Adding the function of copycheck and rm_bk

--- backup.sh
+++ backup.sh
@@ -10,11 +10,23 @@ all_day_2=15
 year=`date +%Y`
 mon=`date +%m`
 day=`date +%d`
-echo $year $mon $day
 
 rm_back(){
 
 	#remove 2 month ago backup file
+	if [ "$1" == "hb" ];then
+                let mon_bk=$mon-2
+       		[ -e $BACKPATH/ubuntu_home_backup@$year-$mon_bk-*.tar.gz ]&& rm $BACKPATH/ubuntu_home_backup@$year-$mon_bk-*.tar.gz
+                [ -e $BACKPATH/ubuntu_book_backup@$year-$mon_bk-*.tar.gz ]&& rm $BACKPATH/ubuntu_book_backup@$year-$mon_bk-*.tar.gz
+
+        elif [ "$1" == "all" ];then
+		let mon_bk=$mon-2
+                [ -e $BACKPATH/ubuntu_backup@$year-$mon_bk-*.tar.gz ]&& rm $BACKPATH/ubuntu_backup@$year-$mon_bk-*.tar.gz
+
+        fi
+        echo "Remove 2 month age backup_File Done!!!"
+        return 1
+
 
 }
 
@@ -22,36 +34,57 @@ rm_back(){
 check_back(){
 	echo "Before"
 	if [ "$1" == "hb" ];then
-		let mon=$mon-1
-		[ -e "$BACKPATH/ubuntu_home_backup@$year-$mon-$day.tar.gz" ]	
+		#let mon_bk=$mon-1
+		[ -e "$BACKPATH/ubuntu_home_backup@$year-$mon-$day.tar.gz" ]&& \
+		[ -e "$BACKPATH/ubuntu_book_backup@$year-$mon-$day.tar.gz" ]&& return 1	
 		
 	elif [ "$1" == "all" ];then
 		
+		[ -e "$BACKPATH/ubuntu_backup@$year-$mon-$day.tar.gz" ]&& return 1
 
 	fi
 	echo "AFTER"
+	return 0
 }
 
 hb_copy(){
 
-	echo "hb"
+	echo "[BackUP] Now going to Back UP /home "
+	tar -cvpzf $BACKPATH/ubuntu_home_backup@`date +%Y-%m-%d`.tar.gz /home
 	
-	#tar -cvpzf $BACKPATH/ubuntu_home_backup@`date +%Y-%m-%d`.tar.gz /home
-	#tar -cvpzf $BACKPATH/ubuntu_boot_backup@`date +%Y-%m-%d`.tar.gz /boot
+	echo "[BackUP] Now going to Back UP /boot "
+	tar -cvpzf $BACKPATH/ubuntu_boot_backup@`date +%Y-%m-%d`.tar.gz /boot
+
+	echo "[BackUP] Back Up /home /boot Done !!"
 }
 
 all_copy(){
-
-	#tar -cvpzf $BACKPATH/ubuntu_backup@`date +%Y-%m+%d`.tar.gz --exclude=/proc --exclude=/tmp --exclude=/boot --exclude=/home --exclude=/lost+found --exclude=/media --exclude=/mnt --exclude=/run /
-	echo "all"
+	
+	echo "[BackUP] Now going to Back UP all file system /"
+	tar -cvpzf $BACKPATH/ubuntu_backup@`date +%Y-%m-%d`.tar.gz --exclude=/proc --exclude=/tmp --exclude=/boot --exclude=/home --exclude=/lost+found --exclude=/media --exclude=/mnt --exclude=/run /
+	echo "[BackUP] Back Up Done !!"
 }
 
 do_main(){
-	echo "Starting Backup_check"
-	[ "$day" == "$hb_day1" -o "$day" == "$hb_day2" ]&& check_back hb && hb_copy
+	echo "[BackUP] Starting Backup_check"
+	
+	[ "$day" = "$hb_day_1" -o "$day" = "$hb_day_2" ]&& check_back hb && hb_copy && rm_back
+	[ "$day" = "$all_day_1" -o "$day" = "$all_day_2" ]&& check_back all && all_copy && rm_back
+	
+	if [ -z "$1"  ]; then
 
-	[ "$day" == "$all_day1" -o "$day" == "$all_day2" ]&& check_back all && all_copy
-	echo "Backup process done"
+        echo $0 "[command]"
+        echo "all -- Backup all files in /."
+        echo "hb  -- Backup files in /home & /boot "
+        echo
+        exit 0
+	fi
+
+	[ "$1" = "all"  ]&& check_back all && all_copy && rm_back
+	[ "$1" = "hb"  ]&& check_back hb && hb_copy && rm_back	
+
+ 
+	echo "[BackUP] Backup process done"
 }
 
-do_main
+do_main $1 
