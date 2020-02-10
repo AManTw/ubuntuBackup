e84a764d4e4df16835d6fbc1412dc547d869c7ef
AManTw <jerr83553@gmail.com>
2020-02-10 15:38:07 +0800

    1. as the month cross to new year, the month -2 and year -1 would be nagtive. fix it

--- backup.sh
+++ backup.sh
@@ -15,55 +15,54 @@ rm_back(){
 
 	#remove 2 month ago backup file
 	echo "[BackUP-7] Remove 2 month age backup_File"
+	mon_bk=`date -d "$(date +%m) -2 month" +%m`
+	year_bk=`date -d "$(date +%Y) -2 month" +%Y`
+	echo $year_bk $mon_bk
 	if [ "$1" == "hb" ];then
-		let mon_bk=$mon-2
-		echo $mon_bk
-		[ -e $BACKPATH/ubuntu_home_backup@$year-$mon_bk-*.tar.gz ]&& {
-			echo "Remove" "$BACKPATH/ubuntu_home_backup@$year-$mon_bk-*.tar.gz"
-			rm -f $BACKPATH/ubuntu_home_backup@$year-$mon_bk-*.tar.gz
+
+		[ -e $BACKPATH/ubuntu_home_backup__$year_bk-$mon_bk-*.tar.gz ]&& {
+			echo "Remove" "$BACKPATH/ubuntu_home_backup__$year_bk-$mon_bk-*.tar.gz"
+			rm -f $BACKPATH/ubuntu_home_backup__$year_bk-$mon_bk-*.tar.gz
 		}
-		[ -e $BACKPATH/ubuntu_boot_backup@$year-$mon_bk-*.tar.gz ]&& { 
-			echo "Remove" "$BACKPATH/ubuntu_boot_backup@$year-$mon_bk-*.tar.gz"	
-			rm -f $BACKPATH/ubuntu_book_backup@$year-$mon_bk-*.tar.gz
+		[ -e "$BACKPATH/ubuntu_boot_backup__$year_bk-$mon_bk-*.tar.gz" ]&& { 
+			echo "Remove" "$BACKPATH/ubuntu_boot_backup__$year_bk-$mon_bk-*.tar.gz"	
+			rm -f $BACKPATH/ubuntu_book_backup__$year_bk-$mon_bk-*.tar.gz
 		}
 
 	elif [ "$1" == "all" ];then
-		let mon_bk=$mon-2
-		[ -e $BACKPATH/ubuntu_backup@$year-$mon_bk-*.tar.gz ]&& {
-			echo "Remove" "$BACKPATH/ubuntu_backup@$year-$mon_bk-*.tar.gz"
-			rm -f $BACKPATH/ubuntu_backup@$year-$mon_bk-*.tar.gz
+		[ -e $BACKPATH/ubuntu_backup__$year_bk-$mon_bk-*.tar.gz ]&& {
+			echo "Remove" "$BACKPATH/ubuntu_backup__$year_bk-$mon_bk-*.tar.gz"
+			rm -f $BACKPATH/ubuntu_backup__$year_bk-$mon_bk-*.tar.gz
 		}
 
 	fi
 	echo "[BackUP-8] Remove backup_File Done!!!"
 	return 1
-
-
 }
 
 
 check_back(){
 	echo "[BackUP-2] Check whether same month backup file."
 	if [ "$1" == "hb" ];then
-		echo "$BACKPATH/ubuntu_home_backup@$year-$mon-$hb_day_1.tar.gz"
+		echo "$BACKPATH/ubuntu_home_backup__$year-$mon-$hb_day_1.tar.gz"
 		
-		[ -e "$BACKPATH/ubuntu_home_backup@$year-$mon-$hb_day_1.tar.gz" ]&& \
-		[ -e "$BACKPATH/ubuntu_boot_backup@$year-$mon-$hb_day_1.tar.gz" ]&& return 0	
-
+		if [ -e "$BACKPATH/ubuntu_home_backup__$year-$mon-$hb_day_1.tar.gz" ] && [ -e "$BACKPATH/ubuntu_boot_backup__$year-$mon-$hb_day_1.tar.gz" ]; then
+			echo "FAIL"
+			exit 0	
+		fi
 	elif [ "$1" == "all" ];then
-		[ -e "$BACKPATH/ubuntu_backup@$year-$mon-$all_day_1.tar.gz" ]&& return 0
+		[ -e "$BACKPATH/ubuntu_backup__$year-$mon-$all_day_1.tar.gz" ]&& exit 0
 	fi
 	echo "[BackUP-3] Check backfile  have done"
-	#return 1
 }
 
 hb_copy(){
 
 	echo "[BackUP-4] Now going to Back UP /home "
-	tar -cvpzf $BACKPATH/ubuntu_home_backup@`date +%Y-%m-%d`.tar.gz  --exclude=/home/jerrychen/.cache/ /home
+	tar -cvpzf $BACKPATH/ubuntu_home_backup_`date +%Y-%m-%d`.tar.gz  --exclude=/home/jerrychen/.cache/ /home  > home.log
 
 	echo "[BackUP-5] Now going to Back UP /boot "
-	tar -cvpzf $BACKPATH/ubuntu_boot_backup@`date +%Y-%m-%d`.tar.gz --exclude=/home/jerrychen/.cache/ /boot
+	tar -cvpzf $BACKPATH/ubuntu_boot_backup_`date +%Y-%m-%d`.tar.gz --exclude=/home/jerrychen/.cache/ /boot > boot.log
 
 	echo "[BackUP-6] Back Up /home /boot Done !!"
 }
@@ -71,12 +70,12 @@ hb_copy(){
 all_copy(){
 
 	echo "[BackUP-4] Now going to Back UP all file system /"
-	tar -cvpzf $BACKPATH/ubuntu_backup@`date +%Y-%m-%d`.tar.gz --exclude=/proc --exclude=/tmp --exclude=/lost+found --exclude=/media --exclude=/mnt --exclude=/run /
+	tar -cvpzf $BACKPATH/ubuntu_backup_`date +%Y-%m-%d`.tar.gz --exclude=/proc --exclude=/tmp --exclude=/lost+found --exclude=/media --exclude=/mnt --exclude=/run / > all.log
 	echo "[BackUP-5] Back Up Done !!"
 }
 
 do_main(){
-
+	
 	if [ "$1" = "help" ]; then
                 echo $0 "[command]"
                 echo "all -- Backup all files in /."
@@ -85,7 +84,7 @@ do_main(){
                 exit 0
         fi
 
-
+	cd /root/ubuntuBackup
 	echo "[BackUP-1] Starting Backup_check"
 	echo $day $hb_day_1
 	[ "$day" = "$hb_day_1" -o "$day" = "$hb_day_2" ]&& check_back hb && hb_copy && rm_back hb 
